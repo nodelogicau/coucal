f58cefdb0b665067ad2663ba179b0f823fdc08e6
Ben Fortuna <fortuna@micronode.com>
2023-04-14 13:15:58 +1000

    Added command skeletons

---
+++ src/main/java/org/coucal/Query.java
@@ -0,0 +1,10 @@
+package org.coucal;
+
+import net.fortuna.ical4j.filter.FilterExpression;
+
+public class Query {
+
+    private String name;
+
+    private FilterExpression filterExpression;
+}
--- src/main/java/org/coucal/command/QueryCommand.java
+++ src/main/java/org/coucal/command/QueryCommand.java
@@ -1,7 +1,7 @@
 package org.coucal.command;
 
 import org.ical4j.connector.command.AbstractCommand;
-import org.mnode.coucou.Query;
+import org.coucal.Query;
 import picocli.CommandLine;
 
 import java.util.List;
